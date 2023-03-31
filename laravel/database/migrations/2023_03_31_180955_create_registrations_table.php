a4bc46c98eb6191741582871eeb17f1017b3076c
Tuur Delacroix <tuur.delacroix@proximus.be>
2023-03-31 20:58:36 +0200

    Created Registration Table Migration

---
+++ laravel/database/migrations/2023_03_31_180955_create_registrations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->id('registration_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->timestamp('registration_time')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registrations');
+    }
+}
